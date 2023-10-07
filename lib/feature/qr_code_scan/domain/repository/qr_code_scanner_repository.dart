fe1c41f29bd42a17a01117a7e4ef3871232623dd
ds <yassinnouh21@gmail.com>
2023-10-07 15:16:50 +0200

    feat(repository):  add repository for qr code scanner

---
+++ lib/feature/qr_code_scan/domain/repository/qr_code_scanner_repository.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:event_plus/feature/qr_code_scan/domain/failure/qr_code_exception.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+abstract interface class QRCodeScannerRepository {
+  /// toggle flash light
+  Future<Either<QRCodeException, Unit>> toggleFlashLight();
+
+  /// Stop the QR code scanning process.
+  Future<Either<QRCodeException, Unit>> stopScanning();
+
+  /// Scan a QR code and return the scanned data as a string.
+  /// return stream of scanned data
+  Either<QRCodeException, Stream<Barcode>> scanQRCode();
+
+  /// Create a QRViewController and return it.
+  Either<QRCodeException, Unit> onCreateQrController(
+    QRViewController controller,
+  );
+
+  /// create a dispose function for QRViewController
+  Either<QRCodeException, Unit> onDisposeQrController();
+
+  /// set permission for camera
+  Either<QRCodeException,Unit> setPermissionForCamera(
+    BuildContext context,
+    QRViewController ctrl,
+    bool permission,
+  );
+}
