3545df4922ee32f4a66f8c619172cc256b614626
ds <yassinnouh21@gmail.com>
2023-10-09 12:17:17 +0200

    feat(common): add app assets for all asset paths

---
+++ lib/common/constant/app_asset.dart
@@ -0,0 +1,5 @@
+class AppAsset {
+  static const String personIcon = 'assets/icon/person.png';
+  static const String eventIcon = 'assets/icon/event.png';
+  static const String cameraIcon = 'assets/icon/camera.png';
+}
